0c34a02f6626367e17312c934c9add9cbdd2fbb4
Hsiaoming Yang <me@lepture.com>
2021-10-14 19:33:03 +0900

    Update registry method

--- build/update.py
+++ build/update.py
@@ -8,6 +8,13 @@ LINK_ANCHOR = re.compile(r'<a[^>]+><svg class="octicon octicon-link".+?</svg></a
 url_base = 'https://api.github.com/repos/'
 
 
+def fetch_theme_info(repo):
+    url = f'https://raw.githubusercontent.com/{repo}/master/theme.json'
+    req = Request(url)
+    with urlopen(req) as f:
+        return json.load(f)
+
+
 def fetch_repo_data(repo):
     url = f'{url_base}{repo}'
     req = Request(url)
@@ -28,29 +35,22 @@ def fetch_readme(repo):
     return LINK_ANCHOR.sub('', content)
 
 
-def update_theme(name):
-    with open(f'registry/{name}') as f:
-        data = json.load(f)
-
-    repo = data['repo']
+def update_theme(name, repo):
+    data = fetch_theme_info(repo)
+    print('update:', name, repo, data['version'])
     info = fetch_repo_data(repo)
     data['avatar'] = info['owner']['avatar_url']
     data['stars'] = info['watchers']
     data['readme'] = fetch_readme(repo)
-    with open(f'data/{name}', 'w') as f:
+    with open(f'data/{name}.json', 'w') as f:
         json.dump(data, f)
 
 
 def main():
-    filenames = sys.argv[1:]
-    for filename in filenames:
-        if filename.startswith('registry/'):
-            name = filename.split('/', 1)[1]
-            try:
-                print('update:', name)
-                update_theme(name)
-            except Exception as error:
-                print('failed:', error)
+    with open('registry.json') as f:
+        data = json.load(f)
+        for name in data:
+            update_theme(name, data[name])
 
 
 if __name__ == '__main__':
